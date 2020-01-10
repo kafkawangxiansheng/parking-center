f5ede15b215ac45ae1f2968c629492c642aa975c
Vincent Cao <vu.cao@mobytelab.com>
2020-01-11 01:07:22 +0700

    update  revenue

--- parking-api/src/main/java/com/spm/service/impl/RevenueServiceImpl.java
+++ parking-api/src/main/java/com/spm/service/impl/RevenueServiceImpl.java
@@ -9,14 +9,11 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.mysql.jdbc.util.VersionFSHierarchyMaker;
 import com.spm.dto.RevenueDto;
 import com.spm.entity.OrderEntity;
 import com.spm.entity.RevenueEntity;
-import com.spm.entity.SettingEntity;
 import com.spm.entity.VehicleEntity;
 import com.spm.repository.OrderRepository;
-import com.spm.repository.SettingRepository;
 import com.spm.repository.VehicleRepository;
 import com.spm.search.form.RevenueSearchForm;
 import com.spm.service.RevenueService;
@@ -46,9 +43,21 @@ public class RevenueServiceImpl implements RevenueService {
 	@Override
 	public List<RevenueDto> getRevenues(RevenueSearchForm revenueSearchForm) {
 		
-		List<RevenueDto> revenues = new ArrayList<>();
+		List<RevenueDto> normalRevenues = new ArrayList<>();
+		List<RevenueDto> monthRevenues = new ArrayList<>();
+		List<RevenueDto> totalRevenues = new ArrayList<>();
+		
 		List<VehicleEntity> vehicles = vehicleRepository.findAllByProjectId(revenueSearchForm.getProjectId());
 		List<OrderEntity> orderEntities  = orderRepository.findAll(String.valueOf(revenueSearchForm.getProjectId()), revenueSearchForm.getEmployeeId(), revenueSearchForm.getDateFrom(), revenueSearchForm.getDateTo());
+		RevenueDto normalRevenueTotalDto = new RevenueDto();
+		normalRevenueTotalDto.setLabel("___ Tổng cộng xe thường");
+		normalRevenueTotalDto.setCssClass("revenue-sub-total");
+		RevenueDto monthlyRevenueTotalDto = new RevenueDto();
+		monthlyRevenueTotalDto.setLabel("___ Tổng cộng xe tháng");
+		monthlyRevenueTotalDto.setCssClass("revenue-sub-total");
+		RevenueDto revenueTotalDto = new RevenueDto();
+		revenueTotalDto.setLabel("Tổng cộng:");
+		revenueTotalDto.setCssClass("revenue-total");
 		
 		for(VehicleEntity vehicle :  vehicles) {
 			RevenueDto revenueDto = new RevenueDto();
@@ -65,11 +74,40 @@ public class RevenueServiceImpl implements RevenueService {
 				mapper.map(revenueEntity, revenueDto);
 			}
 			revenueDto.setLabel(vehicle.getName());
-			revenues.add(revenueDto);
+			revenueDto.setExistingVerhicle(revenueDto.getTotalCheckin() - revenueDto.getTotalCheckout());
+			if(vehicle.getType() == 1)  {
+				normalRevenues.add(revenueDto);
+				normalRevenueTotalDto.setTotalCheckin(normalRevenueTotalDto.getTotalCheckin() + revenueDto.getTotalCheckin());
+				normalRevenueTotalDto.setTotalCheckout(normalRevenueTotalDto.getTotalCheckout() + revenueDto.getTotalCheckout());
+				normalRevenueTotalDto.setExistingVerhicle(normalRevenueTotalDto.getExistingVerhicle() + revenueDto.getExistingVerhicle());
+				normalRevenueTotalDto.setTotalPrice(normalRevenueTotalDto.getTotalPrice()+revenueDto.getTotalPrice());
+			} else {
+				monthRevenues.add(revenueDto);
+				monthlyRevenueTotalDto.setTotalCheckin(monthlyRevenueTotalDto.getTotalCheckin()+revenueDto.getTotalCheckin());
+				monthlyRevenueTotalDto.setTotalCheckout(monthlyRevenueTotalDto.getTotalCheckout()+revenueDto.getTotalCheckout());
+				monthlyRevenueTotalDto.setExistingVerhicle(monthlyRevenueTotalDto.getExistingVerhicle() + revenueDto.getExistingVerhicle());
+				monthlyRevenueTotalDto.setTotalPrice(monthlyRevenueTotalDto.getTotalPrice()+revenueDto.getTotalPrice());
+			}
+			
 			
 		}
+		for(RevenueDto normalReveneu : normalRevenues) {
+			totalRevenues.add(normalReveneu);
+		}
+		totalRevenues.add(normalRevenueTotalDto);
+		for(RevenueDto monthlyReveneu : monthRevenues) {
+			totalRevenues.add(monthlyReveneu);
+		}
+		totalRevenues.add(monthlyRevenueTotalDto);
 		
-		return revenues;
+		revenueTotalDto.setTotalCheckin(normalRevenueTotalDto.getTotalCheckin()  + monthlyRevenueTotalDto.getTotalCheckin());
+		revenueTotalDto.setTotalCheckout(normalRevenueTotalDto.getTotalCheckout()  + monthlyRevenueTotalDto.getTotalCheckout());
+		revenueTotalDto.setExistingVerhicle(normalRevenueTotalDto.getExistingVerhicle()  + monthlyRevenueTotalDto.getExistingVerhicle());
+		revenueTotalDto.setTotalPrice(normalRevenueTotalDto.getTotalPrice()  + monthlyRevenueTotalDto.getTotalPrice());
+		
+		totalRevenues.add(revenueTotalDto);
+		
+		return totalRevenues;
 	}
 
 	
