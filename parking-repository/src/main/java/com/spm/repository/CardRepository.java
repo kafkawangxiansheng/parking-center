c7a0723302035fae4c5a222cb8af6b6df26cc428
Tuan <tuandev76@gmail.com>
2020-01-14 15:37:45 +0700

    update

--- parking-repository/src/main/java/com/spm/repository/CardRepository.java
+++ parking-repository/src/main/java/com/spm/repository/CardRepository.java
@@ -29,5 +29,5 @@ public interface CardRepository  extends JpaRepository<CardsEntity, Long> {
 	@Query(value = "SELECT c.* FROM cards as c INNER JOIN vehicles as v on c.vehicle_id = v.id AND v.card_type = :cardType AND c.code = :code",
 			nativeQuery = true
 		)
-	CardsEntity findByCodeAndVehicleCardType(String code, int cardType);
+	CardsEntity findByCodeAndVehicleCardType(@Param(value = "code") String code, @Param(value = "cardType") int cardType);
 }
