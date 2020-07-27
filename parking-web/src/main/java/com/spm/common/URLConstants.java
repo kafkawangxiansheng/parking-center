0808acb66e445d23f0a4ba68a502db616a4f42a7
Vincent Cao <caohongvu@gmail.com>
2020-07-28 00:21:31 +0700

    update parking config

--- parking-web/src/main/java/com/spm/common/URLConstants.java
+++ parking-web/src/main/java/com/spm/common/URLConstants.java
@@ -1,7 +1,7 @@
 package com.spm.common;
 
 public class URLConstants {
-	private static final String  MAIN_DOMAIN = "http://127.0.0.1:1235";
+	private static final String  MAIN_DOMAIN = "http://localhost:1235";
 	public static final String URL_GET_ALL_IN_OUT = MAIN_DOMAIN + "/orders/?page=::page&employeeId=::employeeId&isMonthlyCard=::isMonthlyCard&projectId=::projectId&cardCode=::cardCode&cardStt=::cardStt&carNumber=::carNumber&dateFrom=::dateFrom&dateTo=::dateTo";
 	public static final String URL_EXPORT_ALL_IN_OUT = MAIN_DOMAIN + "/orders/export/?page=::page&employeeId=::employeeId&isMonthlyCard=::isMonthlyCard&projectId=::projectId&cardCode=::cardCode&cardStt=::cardStt&carNumber=::carNumber&dateFrom=::dateFrom&dateTo=::dateTo";
 	public static final String URL_GET_ALL_REVENUE = MAIN_DOMAIN + "/revenue/?projectId=::projectId&employeeId=::employeeId&dateFrom=::dateFrom&dateTo=::dateTo";
